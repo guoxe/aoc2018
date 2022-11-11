1672657dd8707f7d2aae00a85e88dfbf2f8b4bfc
Gustaf Oxenstierna <gustaf@oxenstierna.org>
2022-11-11 14:43:42 +0100

    Update lib.rs
    WIP day3

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use std::collections::HashSet;
 use itertools::Itertools;
+use regex::Regex;
+use std::{collections::HashSet, num::ParseIntError, str::FromStr};
 
 // ------- DAY 1
 pub fn day1_part1(sequence: impl Iterator<Item = i32>) -> i32 {
@@ -79,12 +80,66 @@ pub fn day2_part2(lines: impl Iterator<Item = String>) -> Option<String> {
     for pair in lines.combinations(2) {
         let probe = &pair[0];
         let candidate = &pair[1];
-        let result = probe.chars().zip(candidate.chars()).filter(|(a, b)| a == b).map(|(a, _)| a).collect::<String>();
+        let result = probe
+            .chars()
+            .zip(candidate.chars())
+            .filter(|(a, b)| a == b)
+            .map(|(a, _)| a)
+            .collect::<String>();
         if result.len() == probe.len() - 1 {
-            return Some(result)
+            return Some(result);
         }
     }
     None
+}
+
+// ------- DAY 3
+
+struct Claim {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
+
+impl Claim {
+    fn union(other: Self) -> u16 {
+        0
+    }
+}
+
+enum ClaimErr {
+    BadInput(String),
+    ParseIntError,
+}
+
+impl From<ParseIntError> for ClaimErr {
+    fn from(_: ParseIntError) -> Self {
+        Self::ParseIntError
+    }
+}
+
+impl FromStr for Claim {
+    type Err = ClaimErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let re = Regex::new(r"#(?P<id>\d+)\s@\s(?P<x>\d+),(?P<y>\d+):\s(?P<w>\d+)x(?P<h>\d+)")
+            .expect("Invalid regex");
+        let caps = re.captures(s).unwrap();
+        if caps.name("x").is_none()
+            || caps.name("y").is_none()
+            || caps.name("w").is_none()
+            || caps.name("h").is_none()
+        {
+            return Err(ClaimErr::BadInput("Missing fields in line".to_owned()));
+        }
+        Ok(Self {
+            x: caps.name("x").unwrap().as_str().parse::<u16>()?,
+            y: caps.name("y").unwrap().as_str().parse::<u16>()?,
+            w: caps.name("w").unwrap().as_str().parse::<u16>()?,
+            h: caps.name("h").unwrap().as_str().parse::<u16>()?,
+        })
+    }
 }
 
 #[cfg(test)]
@@ -186,5 +241,16 @@ mod tests {
             .lines()
             .map(|l| l.unwrap());
         assert_eq!(day2_part2(input).unwrap(), "pebjqsalrdnckzfihvtxysomg");
+    }
+
+    #[test]
+    fn day3_part1_small() {
+        let input = vec!["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"];
+        for l in input {
+            if let Ok(claim) = Claim::from_str(l) {
+                println!("{}", claim.x);
+            }
+        }
+        assert!(false);
     }
 }
